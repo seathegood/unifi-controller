651f8ad86c219f64139ed42a82101eeda779db2d
seathegood <michael@seathegood.us>
2025-08-06 17:08:29 -0500

    chore(repo): refine entrypoint and healthcheck script to meet newer standards

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,33 +1,34 @@
+# shellcheck shell=bash
 #!/usr/bin/env bash
 
 # healthcheck.sh script for Unifi running within a container
 # License: MIT
-HEALTHCHECK_VERSION="1.0"
-# Last updated date: 2021-11-15
+SCRIPT_VERSION="1.1"
+# Last updated date: 2025-08-06
+
+set -Eeuo pipefail
+
+if [ "${DEBUG}" == 'true' ]; then
+    set -x
+fi
 
 BASEDIR="/usr/lib/unifi"
-DATADIR=${BASEDIR}/data
+DATADIR="${BASEDIR}/data"
 
-[ ! -f ${DATADIR}/system.properties ] || api_port=$(grep "^[^#;]" ${DATADIR}/system.properties | sed -n 's/unifi.http.port=\([0-9]\+\)/\1/p')
+if [[ -f "${DATADIR}/system.properties" ]]; then
+    api_port=$(grep -E '^\s*unifi\.http\.port=' "${DATADIR}/system.properties" | sed -n 's/.*=//p')
+fi
+api_port="${api_port:-8080}"
 
-api_port=${api_port:-8080}
-
-http_code=$(curl -s --connect-timeout 1 -o /dev/null -w "%{http_code}" http://localhost:${api_port}/status)
-
-#MAX_WAIT=25
-
-#for i in `seq 1 ${MAX_WAIT}` ; do
-#    if [ "${http_code}" != "200" ]; then
-#        sleep 1 http_code=$(curl -s --connect-timeout 1 -o /dev/null -w "%{http_code}" http://localhost:${api_port}/status)
-#    else
-#        break
-#    fi
-#done
-
-if [ "${http_code}" != "200" ]; then
-        echo "Error: Unifi API http status code ${http_code}, expecting 200"
-        exit 1
-    else
+max_wait=10
+for i in $(seq 1 "${max_wait}"); do
+    http_code=$(curl -s --connect-timeout 1 -o /dev/null -w "%{http_code}" "http://localhost:${api_port}/status")
+    if [[ "${http_code}" == "200" ]]; then
         echo "Unifi API http status code ${http_code}: OK"
         exit 0
-fi
+    fi
+    sleep 1
+done
+
+echo "Error: Unifi API http status code ${http_code}, expecting 200" >&2
+exit 1
